205be2dfae295319e24ce85b87ec14ecf874202d
Daniel Boghossian <gosianinc@gmail.com>
2023-08-06 12:07:20 -0700

    updated imports in js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from "./node_modules/three/build/three.module.js";
+//import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // import space from './assets/images/space.jpg';
 // import headshot from './assets/images/headshot.jpg ';
