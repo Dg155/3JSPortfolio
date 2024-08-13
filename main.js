9f2c86c409be0f16adba5ef26c057ea213ecc6eb
dg155 <danielboghoss@gmail.com>
2024-08-13 14:48:34 -0700

    Added header button, casette tape, and updated fun facts text

--- main.js
+++ main.js
@@ -53,28 +53,35 @@ composer.addPass(renderScene);
 
 // Update Trivia Text
 
-const TriviaText = ["A custom-made demo of SimCity 2000 is exhibited in the New York Museum of Modern Art.", "The dragon Paarthurnax in The Elder Scrolls V: Skyrim is voiced by the same man who voices Nintendo’s Mario.", "Rockstar Games hired real-life gang members to voice background characters in Grand Theft Auto V.", "Counter-Strike was initially a custom mod you could play in Half-Life.", "In 2012 the developers of Terraria tried to cancel further updates to the game.", "In the first 24 hours of its release, more than a million people logged into Xbox Live to play Halo 3.", "There are five different forms that MissingNo. can take in the original Pokémon GameBoy games.", "The Colossi in Shadow of the Colossus don’t actually have names.", "If you click the units enough times in Warcraft III, they will get annoyed at you and start talking back.", "Donkey Kong 64‘s DK Rap started as a joke between the game designers."];
+const TriviaText = ["The first video game I ever played was Super Mario World on the Nintendo DS.", "My favorite game is The Binding Of Isaac, with over 800 hours under my belt.", "I can play jazz piano and want to open up my own jazz bar one day.", "I eat anywhere from 5-10 Persian cucumbers a day.", "I have a black belt in karate.", "I have a motorcycle license and have go riding once in a while.", "I have gone sky diving, and I would do it again.", "I have a pet chameleon. His name is Hue."]
 var randomElementIndex = Math.floor(Math.random() * TriviaText.length);
 var randomElement = TriviaText[randomElementIndex];
 
 var DYKText = document.getElementById("DYKText");
-DYKText.innerText = "Did You Know: " + randomElement;
 var DYKButton = document.getElementById("more-arrows");
-DYKButton.addEventListener("click", updateDYKText);
+
+DYKText.innerText = "Fun Facts About Me: " + randomElement;
+DYKText.style.opacity = "0";
+DYKText.style.transition = "opacity 0.5s";
+
+setTimeout(() => {
+    DYKText.style.opacity = "1";
+    DYKButton.addEventListener("click", updateDYKText);
+}, 1000);
 
 function updateDYKText() {
-    DYKText.style.transition = "opacity 1s";
     DYKText.style.opacity = "0";
     DYKButton.removeEventListener("click", updateDYKText);
     setTimeout(() => {
         const newTriviaText = [...TriviaText];
+        randomElementIndex = newTriviaText.indexOf(randomElement);
         newTriviaText.splice(randomElementIndex, 1);
         randomElementIndex = Math.floor(Math.random() * newTriviaText.length);
         randomElement = newTriviaText[randomElementIndex];
-        DYKText.innerText = "Did You Know: " + randomElement;
+        DYKText.innerText = "Fun Facts About Me: " + randomElement;
         DYKText.style.opacity = "1";
         DYKButton.addEventListener("click", updateDYKText);
-    }, 1);
+    }, 500);
 }
 
 
@@ -446,7 +453,7 @@ function showScreenAfterLoad() {
     progressBarContainer.style.display = 'none';
     document.getElementById("main").style.display = "block";
     console.log("loaded");
-  }
+}
 
 
 function resizeRendererToDisplaySize(renderer) {
